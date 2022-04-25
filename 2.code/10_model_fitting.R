a0a2113e9ccd12b12cdf2d43d22f764adccf540e
Tom <t.clegg17@imperial.ac.uk>
2022-04-25 09:55:14 +0100

    Yfitting model to all buffer sizes

--- 2.code/10_model_fitting.R
+++ 2.code/10_model_fitting.R
@@ -1,80 +1,66 @@
 library(tidyverse)
 library(brms)
 
-FA_FC_data <- read_csv("FA_FC_final.csv")
+#load data and transform
+FA_FC_data <- read_csv("FA_FC_final.csv") %>%
+    mutate(FC_proportion = FC_percentage.value/100, log_dist = log(dist_km)) %>%  #add columns
+    filter(!is.na(FC_buffer))
 
 FA_FC_data %>% head()
 
 set.seed(1)
 
-bay_mod_t <- FA_FC_data %>%
-mutate(ind_size_fa = (mean_l + mean_r)/2)%>%
-filter(FC_buffer == 600, type == "t", dist_km >= 0) %>%
-brm(bf(fa_left_right ~ 0, sigma ~ log(dist_km) + (log(dist_km) | species)),
-   data = .,
-   family = gaussian,
-   cores = 4, chains = 4, iter = 10000, 
-   control=list(adapt_delta = 0.99))
+#fit models
+models <- list()
 
-bay_mod_w <- FA_FC_data %>%
-mutate(ind_size_fa = (mean_l + mean_r)/2)%>%
-filter(FC_buffer == 600, type == "w", dist_km >= 0) %>%
-brm(bf(fa_left_right ~ 0, sigma ~ log(dist_km) + (log(dist_km) | species)),
-   data = .,
-   family = gaussian,
-   cores = 4, chains = 4, iter = 10000, 
-   control=list(adapt_delta = 0.99))
+#get buffers and trait vectors
+buffers <- unique(FA_FC_data$FC_buffer)
+types <- unique(FA_FC_data$type)
 
-#FC
-bay_mod_t_FC <- FA_FC_data %>%
-    filter(FC_buffer == 600, type == "t") %>%
-    mutate(FC_proportion = FC_percentage.value/100) %>%
-    brm(bf(fa_left_right ~ 0, sigma ~ FC_proportion * log(dist_km) + (log(dist_km) | species)),
-        data = .,
-        family = gaussian,
-        cores = 4, chains = 4, iter = 10000, 
-        control=list(adapt_delta = 0.99))
+#loop over both and fit all models
+k = 1
+for(i in buffers){
+    for(j in types){
+        print(paste(i, j, k / (length(buffers) * length(types))))
+       models[[paste(i,j, sep = " ", " ")]] <- FA_FC_data %>%
+            filter(FC_buffer == i, type == j) %>%
+            brm(bf(fa_left_right ~ 0, sigma ~ FC_proportion * log(dist_km) + (log(dist_km) | species)),
+                data = .,
+                family = gaussian,
+                cores = 4, chains = 4, iter = 10000, 
+                control=list(adapt_delta = 0.99),
+                silent = 2)
+        k <- k+1
+                
+    }
+}
 
-bay_mod_w_FC <- FA_FC_data %>%
-    filter(FC_buffer == 600, type == "w") %>%
-    mutate(FC_proportion = FC_percentage.value/100) %>%
-    brm(bf(fa_left_right ~ 0, sigma ~ FC_proportion * log(dist_km) + (log(dist_km) | species)),
-        data = .,
-        family = gaussian,
-        cores = 4, chains = 4, iter = 10000, 
-        control=list(adapt_delta = 0.99))
+#save output
+save(models, file =  "./2.results/bayesmodels_buffers.RData")
 
-save(bay_mod_t,bay_mod_w,bay_mod_t_FC,bay_mod_w_FC,file = "./2.results/bayesmodels.RData")
+#initial analysis
+load("./2.results/bayesmodels_buffers.RData")
 
-# load("./2.results/bayesmodels.RData")
+#get estimates
+est_list <- list()
 
-# dist <- log(seq(min(FA_FC_data$dist_km), max(FA_FC_data$dist_km),length.out = 100))
-# sp <- FA_FC_data %>%
-#     filter(species != "Thalurania_glaucopis") %>%
-#     pull(species) %>%
-#     unique()
+for(i in 1:length(models)){
+    summary_x <- summary(models[[i]]) #summary object
+    df <- summary_x$fixed #get fixed effects
+    df$name <- names(models)[i] #get the buffer/trait name from the list
+    df$param <- row.names(df) #add the parameters
+    row.names(df) <- NULL
+    df <- df %>%
+        tidyr::separate(name,c("buffer","trait")) #sepateate name into buffer size and trait
 
-# pred_df <- list()
-# for(i in 1:length(sp)){
-#     df_sub <- FA_FC_data %>% filter(species == sp[i])
-#     pred_df[[i]] <- tibble(species = sp[i],
-#     dist_km = log(seq(min(df_sub$dist_km),max(df_sub$dist_km),length.out = 10)))
+    est_list[[i]] <- df
+}
 
-# }
-
-# pred_df <- bind_rows(pred_df)
-# pred_df <- cbind(pred_df,predict(bay_mod_w, newdata = pred_df)) %>%
-#     as_tibble()
-
-# p <- ggplot() +
-#   geom_point(data = FA_FC_data %>% filter(type == "w", species != "Pyriglena_leucoptera"), aes(x = log(dist_km), y = fa_left_right), shape=3) + 
-#   scale_x_continuous(expand=c(0,0)) +
-#   scale_y_continuous(expand=c(0,0)) +
-#   labs(y=NULL, x = NULL)
-#     # facet_wrap(~species, scales = "free")
-
-# p +
-#     geom_ribbon(data = pred_df %>% filter(species != "Pyriglena_leucoptera"), 
-#     aes(x = (dist_km), ymin = Q2.5, ymax = Q97.5, group = species),
-#     alpha = 0.2)
-
+#plot with errorbars
+bind_rows(est_list) %>%
+    filter(abs(Rhat - 1) < 0.1) %>%
+    mutate(buffer = as.numeric(buffer)) %>%
+    ggplot(aes(x=buffer,y=Estimate))+
+        geom_point()+
+        geom_errorbar(aes(ymin = `l-95% CI`, ymax = `u-95% CI`))+
+        facet_grid(param ~ trait, scales = "free")
